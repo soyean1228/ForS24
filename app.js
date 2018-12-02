12438d87889afd24228dba62a1cf902ca1a72951
zionyun <41980125+zionyun@users.noreply.github.com>
2018-12-02 23:17:08 +0900

    bug fix

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('view engine', 'pug');
 
 // // mongodb connect
 mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
-const connStr = 'mongodb://localhost:27017/fors';
+const connStr = 'mongodb://fors:fors1234@ds157971.mlab.com:57971/fors';
 mongoose.connect(connStr, { useNewUrlParser: true });
 mongoose.connection.on('error', console.error);
 
